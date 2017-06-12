6c0b41ca9a1ccf64a565d4f15c1741c92c33c360
AntonioLoureiro <ajose.loureiro@gmail.com>
2017-06-12 16:16:21 +0100

    changed libmongoc to v1.3.5

--- deps/build.jl
+++ deps/build.jl
@@ -8,12 +8,12 @@ libmongoc = library_dependency(
     )
 
 provides(Sources, Dict(
-    URI("http://github.com/mongodb/mongo-c-driver/releases/download/1.0.0/mongo-c-driver-1.0.0.tar.gz") => libmongoc
+    URI("http://github.com/mongodb/mongo-c-driver/releases/download/1.3.5/mongo-c-driver-1.3.5.tar.gz") => libmongoc
     ))
 
 provides(
     BuildProcess,
-    Autotools(libtarget = "libmongoc-1.0.la"),
+    Autotools(libtarget = "libmongoc-1.3.la"),
     libmongoc,
     os = :Unix
     )
