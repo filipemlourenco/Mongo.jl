0d5372fdb4ae4c4f156a8762959d7b6775564035
AntonioLoureiro <ajose.loureiro@gmail.com>
2017-06-22 21:16:33 +0100

    Create aggregate.jl

---
+++ src/aggregate.jl
@@ -0,0 +1,18 @@
+############ Mongo Aggregate (requires mongoc v1.3.5)#################################
+function mongo_aggregate(collection::Mongo.MongoCollection,pipeline::Array,options::Dict=Dict("allowDiskUse"=>false))
+
+pipe_dict=LibBSON.BSONObject(OrderedDict("pipeline"=>pipeline))
+
+options=LibBSON.BSONObject(options)
+
+agg=ccall((:mongoc_collection_aggregate, Mongo.libmongoc), Ptr{Void}, (Ptr{Void},Cint,Ptr{Void},Ptr{Void},Ptr{Void}),
+ collection._wrap_,
+0,
+pipe_dict._wrap_,
+options._wrap_,
+C_NULL
+)
+
+    return Mongo.MongoCursor(agg)
+
+end
